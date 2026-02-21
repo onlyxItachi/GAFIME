8b810ec8f739c93836c7f1a3a5fe7eea1ba9838a
onlyxItachi <hamzausta2222@gmail.com>
2026-02-21 13:14:12 +0300

    fix(windows-wheel): export PyInit__native symbol and fix BOM char in MANIFEST.in

--- gafime/_dummy.c
+++ gafime/_dummy.c
@@ -1,1 +1,11 @@
-﻿int dummy() { return 0; }
+﻿#include <Python.h>
+static struct PyModuleDef dummy_module = {
+    PyModuleDef_HEAD_INIT,
+    "_native",
+    "Dummy extension block",
+    -1,
+    NULL, NULL, NULL, NULL, NULL
+};
+PyMODINIT_FUNC PyInit__native(void) {
+    return PyModule_Create(&dummy_module);
+}
